64e47c227d510cb800ee13204c5d0090f3961f2f
Umid Almatov <almatov.us@gmail.com>
2017-10-02 21:01:42 +0500

    Create form model with FormBuilder

--- Demo-Start/app/customers/customer.component.ts
+++ Demo-Start/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -11,12 +11,14 @@ export class CustomerComponent implements OnInit {
     customerForm: FormGroup;
     customer: Customer= new Customer();
 
+    constructor(private fb: FormBuilder) { }
+
     ngOnInit(): void {
-        this.customerForm = new FormGroup({
-            firstName: new FormControl(),
-            lastName: new FormControl(),
-            email: new FormControl(),
-            sendCatalog: new FormControl(true)
+        this.customerForm = this.fb.group({
+            firstName: '',
+            lastName: '',
+            email: '',
+            sendCatalog: true
         });
     }
 

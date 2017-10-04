1e1232efbd3927e55a76e26b6045e6a4eac8d089
Umid Almatov <almatov.us@gmail.com>
2017-10-04 10:03:35 +0500

    Custom validator example

--- Demo-Start/app/customers/customer.component.ts
+++ Demo-Start/app/customers/customer.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 import { Customer } from './customer';
+
+function ratingRange(c: AbstractControl): {[key: string]: boolean} | null {
+    if (c.value !== undefined && (isNaN(c.value) || c.value < 1 || c.value > 5)) {
+        return {'range': true};
+    }
+
+    return null;
+}
 
 @Component({
     selector: 'my-signup',
@@ -20,6 +28,7 @@ export class CustomerComponent implements OnInit {
             email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+')]],
             phone: '',
             notification: 'email',
+            rating: ['', ratingRange],
             sendCatalog: true
         });
     }

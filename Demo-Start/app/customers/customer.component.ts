bb87301eddb95c9d3b23b37c26882de21e707293
Umid Almatov <almatov.us@gmail.com>
2017-10-04 10:18:26 +0500

    Custom validator with parameters

--- Demo-Start/app/customers/customer.component.ts
+++ Demo-Start/app/customers/customer.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 import { Customer } from './customer';
 
-function ratingRange(c: AbstractControl): {[key: string]: boolean} | null {
-    if (c.value !== undefined && (isNaN(c.value) || c.value < 1 || c.value > 5)) {
-        return {'range': true};
-    }
+function ratingRange(min: number, max: number): ValidatorFn {
+    return (c: AbstractControl): {[key: string]: boolean} | null => {
+        if (c.value !== undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
+            return {'range': true};
+        }
 
-    return null;
+        return null;
+    };
 }
 
 @Component({
@@ -28,7 +30,7 @@ export class CustomerComponent implements OnInit {
             email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+')]],
             phone: '',
             notification: 'email',
-            rating: ['', ratingRange],
+            rating: ['', ratingRange(1, 5)],
             sendCatalog: true
         });
     }
